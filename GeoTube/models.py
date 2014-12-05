4ba7e2634611b2403c9551cb2b3d562c352f4b25
csmitc4829 <csmitc4829@ung.edu>
2014-12-04 19:01:46 -0500

    Added model for user to add information to map.
    UserInput model to include:
    username
    following
    address
    geom

--- GeoTube/models.py
+++ GeoTube/models.py
@@ -1,6 +1,34 @@
-from django.db import models
-from apiclient.discovery import build
+from django.contrib.gis.db import models
+from django.contrib.gis import admin
 
-# Create your models here.
 
-#class RawRequest(models.Model):
+
+class RawRequest(models.Model):
+    Channel = models.CharField(max_length=75)
+    Start_Date = models.CharField(max_length=50)
+    End_Date = models.CharField(max_length=50)
+    Metrics = models.CharField(max_length=100)
+    Dimensions = models.CharField(max_length=100)
+    Filters = models.CharField(max_length=100)
+    Max_Results = models.CharField(max_length=50)
+    Sort = models.CharField(max_length=50)
+    Start_Index = models.CharField(max_length=50)
+    Fields = models.CharField(max_length=50)
+
+    Status = models.CharField(max_length=25, default='Submitted')
+
+    def __unicode__(self):
+        return self.Channel
+
+
+class UserInput(models.Model):
+    user = models.CharField("User name", max_length=50)
+    following = models.IntegerField("Following ", blank=True)
+    address = models.IntegerField("Point address", blank=True)
+    geom = models.PointField(srid=4326)
+
+    def __str__(self):
+        return "{}".format(self.name)
+
+
+admin.site.register(UserInput, admin.OSMGeoAdmin)
